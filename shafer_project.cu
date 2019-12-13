efc4d4306a87817d9e17de68cf42c90b7d73c327
Luke Shafer <lshafer@owens-rw01.ten.osc.edu>
2019-12-13 15:10:29 -0500

    updates

--- shafer_project.cu
+++ shafer_project.cu
@@ -1,9 +1,15 @@
 /*
+ *  Parallel Implementaion of Hough Transform for Circle Detection in images
  *
- *
- *
- *
+ *  by Luke Shafer
+ *  https://github.com/lukeshafer/parallel_project
  */
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 int main( void ) {
 
